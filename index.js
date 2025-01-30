3a2b479a34cefbc76f6da684e8b1ff9a1ff898e9
Mhd.Rabeai <106599666+MhdRabeai@users.noreply.github.com>
2025-01-30 16:52:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use("/", express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://colorgenerator-tawny.vercel.app",
     credentials: true,
   })
 );
